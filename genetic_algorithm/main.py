79bd4ae43343598d56a0afe612a7bfff2fc7dca4
lukaszjecek <lukasz.jecek04@gmail.com>
2025-11-11 18:35:29 +0100

    Add argparse for genetic algorithm parameters
    Replaced hardcoded CSV loading and print statements with argparse-based command-line interface for configuring genetic algorithm parameters and input file. Added validation for argument values.

--- genetic_algorithm/main.py
+++ genetic_algorithm/main.py
@@ -1,7 +1,35 @@
 import pandas as pd
+import argparse
 
-data_bad = pd.read_csv('problem plecakowy dane CSV tabulatory.csv', sep='\t')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-print(data_bad.shape)
-print(data_bad.head())
-print(data_bad.tail())
+    parser.add_argument('--Pc', type=float, default=0.6, help="Prawdopodobieństwo krzyżowania")
+    parser.add_argument('--Pm', type=float, default=0.05, help="Prawdopodobieństwo mutacji")
+    parser.add_argument('--N', type=int, default=30, help="Wielkość populacji")
+    parser.add_argument('--T', type=int, default=300, help="Liczba iteracji")
+    parser.add_argument('--selection_method', type=str, choices=["roulette", "ranking", "tournament"],
+                        help="Metoda selekcji nowego rodzica")
+    parser.add_argument('--crossover_method', type=str, choices=["one_point", "two_point", "uniform"],
+                        help="Metoda krzyżowania w procesie tworzenia potomstwa")
+    parser.add_argument('--mutation_method', type=str, choices=["single_bit_flip", "all_bit_inversion"],
+                        default="single_bit_flip", help="Metoda mutacji genów w chromosomie")
+    parser.add_argument('--data_file', type=str, default="problem plecakowy dane CSV tabulatory.csv",
+                        help="Plik csv z dostępnymi do włożenia do plecaka przedmiotami")
+
+    args = parser.parse_args()
+
+    if args.Pc < 0 or args.Pc > 1.0:
+        parser.error("Prawdopodobieństwo krzyżowaniu musi być w zakresie [0, 1].")
+
+    if args.Pm < 0 or args.Pm > 1.0:
+        parser.error("Prawdopodobieństwo mutacji musi być w zakresie [0, 1].")
+
+    if args.N <= 0:
+        parser.error("Wielkość populacji musi być większa od 0.")
+
+    if args.T <= 0:
+        parser.error("Liczba iteracji musi być większa od 0.")
+
+    data = pd.read_csv(args.data_file, sep='\t')
+    print(f"Wczytano {len(data)} wierszy z pliku: {args.data_file}")
